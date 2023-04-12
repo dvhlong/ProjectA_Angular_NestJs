a037422fe8a6d136b1026e5eaa59fb62e4090fb5
Đoàn Văn Hoàng Long (Ngủ Gục) <long59915@gmail.com>
2023-04-12 17:54:12 +0700

    Configure first component

--- test_angular/src/app/app.component.ts
+++ test_angular/src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.style.css']
 })
 export class AppComponent {
-  title = 'test_angular';
+  title = 'ProjectA';
 }
---
+++ test_angular/src/app/login/login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { AppComponent } from '../app.component';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent extends AppComponent{
+
+}
